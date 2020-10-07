bc2b8c69fa5f32c38fd7f90bc34f8f6443113100
gotokouichi <koichi.110913@icloud.com>
2020-10-08 08:25:33 +0900

    before change package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Comment } from './class/chat'; // 追加
+
+const COMMENTS: Comment[] = [ // 追加
+  { name: "Suzuki Taro",  content: "１つ目のコメントです。"},
+  { name: "Suzuki Taro",  content: "２つ目のコメントです。"},
+  { name: "Suzuki Taro",  content: "３つ目のコメントです。"}
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-practice-chat';
+  public content = '';
+  public comments = COMMENTS; // 追加
 }
